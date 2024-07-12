45e7f4cc02330739f574e7fb042e312afa62149b
Erica Siegel <siegel.erica@gmail.com>
2024-07-12 10:19:37 -0700

    connected to mySQL database

--- server/matrix/settings.py
+++ server/matrix/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = "matrix.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "menuMatrix",
+        "HOST": "localhost",
+        "USER": "root",
+        "PASSWORD": "SiegirlRootpw213",
     }
 }
 
